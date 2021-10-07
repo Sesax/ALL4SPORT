111ac63cb559d6ea856a9bb5a1c5bf05611e3b34
sartek430 <hugo.vayr@gmail.com>
2021-10-07 15:30:50 +0200

    hugo  creation tableau

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,8 +1,105 @@
-import {Button} from '@mui/material'
-function App() {
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+function createData1(name, calories, fat, carbs, protein, truc) {
+  return { name, calories, fat, carbs, protein, truc };
+}
+
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const rows1 = [
+  createData1('Frozen yoghurt', 159, 6.0, 24, 4.0, 12),
+  createData1('Ice cream sandwich', 237, 9.0, 37, 4.3, 54),
+  createData1('Eclair', 262, 16.0, 24, 6.0, 45),
+  createData1('Cupcake', 305, 3.7, 67, 4.3, 45),
+  createData1('Gingerbread', 356, 16.0, 49, 3.9, 897),
+];
+
+export default function FicheEntrpot() {
   return (
-    <Button variant="contained">Hello World</Button>
+
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Fiche entrepot</TableCell>
+            <TableCell align="right">Batiment</TableCell>
+            <TableCell align="right">Rayon</TableCell>
+            <TableCell align="right">Rangée</TableCell>
+            <TableCell align="right">Etagère</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Fiche produit</TableCell>
+            <TableCell align="right">Coût unitaire</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Fournisseur</TableCell>
+            <TableCell align="right">Photo</TableCell>
+            <TableCell align="right">Stock</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows1.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.truc}</TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
-export default App;
+
+
+
+
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import LoginIn from './LoginIn';
+
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-  <App />
-  <LoginIn />
+    <App />
+
   </BrowserRouter>,
   document.getElementById('root')
 );
